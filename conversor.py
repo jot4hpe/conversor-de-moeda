8166564542d7aef4a6df06d5a69ea1e67b736ac7
jot4hpe <jot4hpe.com>
2024-05-06 13:51:26 -0300

    commit conversor

---
+++ conversor.py
@@ -0,0 +1,7 @@
+perg = str(input('voce quer comprar doláres?: '))
+inf = str (input('o dólar está valendo R$3.27 ok? '))
+din = float(input('quantos R$ voce tem?: '))
+dol = din/3.27
+print(f'voce pode comprar {dol:.2f} dolares')
+
+
